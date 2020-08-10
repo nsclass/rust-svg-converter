4294660166602aecd8ca53f44aa1d0b19d87f3b8
Nam Seob Seo <nsclass@gmail.com>
2020-08-10 09:33:12 -0500

    adding a test

--- svg-converter/src/domain/image_data.rs
+++ svg-converter/src/domain/image_data.rs
@@ -8,9 +8,9 @@ pub struct ImageData {
 impl ImageData {
     pub fn new(width: usize, height: usize, data: Vec<u8>) -> Self {
         Self {
-            width: width,
-            height: height,
-            data: data,
+            width,
+            height,
+            data,
         }
     }
 
@@ -20,5 +20,22 @@ impl ImageData {
 
     pub fn index_at(&self, idx: usize) -> u8 {
         self.data[idx]
+    }
+
+    pub fn index_at_row_col(&self, row: usize, col: usize) -> u8 {
+        self.data[row * self.width + col]
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_index_at() {
+        // 2 x 4 matrix
+        let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
+        let image_data = ImageData::new(4, 2, data);
+        assert_eq!(image_data.index_at_row_col(0, 1), 2);
+        assert_eq!(image_data.index_at_row_col(1, 2), 7);
     }
 }
