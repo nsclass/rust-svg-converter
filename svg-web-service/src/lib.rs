89615aa4c75b5e336a5048fa52fbc28d45d89f59
Nam Seob Seo <nsclass@gmail.com>
2020-10-05 20:42:03 -0500

    actix tracing

--- svg-web-service/src/lib.rs
+++ svg-web-service/src/lib.rs
@@ -3,15 +3,18 @@ mod web_handler;
 
 use crate::config::Config;
 use actix_web::dev::Server;
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{App, HttpServer};
 use color_eyre::Result;
 use tracing::info;
 use web_handler::app_config;
+use tracing_actix_web::TracingLogger;
 
 pub fn app_run(conf: Config) -> Result<Server, std::io::Error> {
     info!("starting server at http://{}:{}", conf.host, conf.port);
 
-    let server = HttpServer::new(move || App::new().wrap(Logger::default()).configure(app_config))
+    let server = HttpServer::new(move || App::new()
+        .wrap(TracingLogger)
+        .configure(app_config))
         .bind(format!("{}:{}", conf.host, conf.port))?
         .run();
     Ok(server)
