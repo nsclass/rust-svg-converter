535c54af9faae5a939a7fe4f6ece94651728576c
Nam Seob Seo <nsclass@gmail.com>
2020-10-05 20:27:47 -0500

    tracing

--- svg-web-service/src/config/mod.rs
+++ svg-web-service/src/config/mod.rs
@@ -7,8 +7,43 @@ use color_eyre::Result;
 use dotenv::dotenv;
 use eyre::WrapErr;
 use serde::Deserialize;
-use tracing::{info, instrument};
-use tracing_subscriber::EnvFilter;
+use tracing::{info, instrument, Subscriber};
+use tracing_subscriber::{EnvFilter, Registry};
+use tracing_log::LogTracer;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+
+/// We are using `impl Subscriber` as return type to avoid having to
+/// spell out the actual type of the returned subscriber, which is
+/// indeed quite complex.
+/// We need to explicitly call out that the returned subscriber is
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+/// later on.
+pub fn get_subscriber(
+    name: String,
+    env_filter: String
+) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(
+        name.into(),
+        std::io::stdout
+    );
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) -> Result<()> {
+    LogTracer::init().context("set a logger")?;
+    set_global_default(subscriber).context("set a global subscriber")?;
+    Ok(())
+}
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
@@ -20,10 +55,8 @@ impl Config {
     #[instrument]
     pub fn from_env() -> Result<Self> {
         dotenv().ok();
-
-        let _ = tracing_subscriber::fmt()
-            .with_env_filter(EnvFilter::from_default_env())
-            .try_init();
+        let subscriber = get_subscriber("svg_converter".into(), "info".into());
+        init_subscriber(subscriber)?;
 
         info!("loading configuration");
         let mut cfg = config::Config::new();
--- svg-web-service/src/main.rs
+++ svg-web-service/src/main.rs
@@ -5,10 +5,11 @@
 
 use color_eyre::Result;
 use svg_web_service::app_run;
-use svg_web_service::config::Config;
+use svg_web_service::config::{Config};
 
 #[actix_rt::main]
 async fn main() -> Result<()> {
+
     let conf = Config::from_env()?;
     let _ = app_run(conf)?.await;
 
